2a28149604c4e1c66cc915d874c26748bf6be4fd
Arthur <arthureboh@uma.es>
2022-03-17 11:36:38 +0100

    Update DequeNodeTest.java

--- src/test/java/DequeNodeTest.java
+++ src/test/java/DequeNodeTest.java
@@ -5,12 +5,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class DequeNodeTest {
 
     @Test
-    public void comprobarValorConNodoConValorYConectadoConAnteriorYPosterior(){
-        DequeNode<Integer> anterior = new DequeNode<>(1,null,null);
-        DequeNode<Integer> posterior = new DequeNode<>(2, null, null);
-        DequeNode<Integer> node = new DequeNode<>(1, anterior, posterior);
-        assertEquals(posterior,node.getNext());
-        assertEquals(anterior,node.getPrevious());
-        assertEquals(1,node.getItem());
+    public void NodoSeCreaConValorCorrecto()
+    {
+        DequeNode<Integer> nodo = new DequeNode(2, null, null);
+
+        int expectedValue = 2;
+        int obtainedValue = nodo.getItem();
+        assertEquals(expectedValue, obtainedValue);
     }
+
 }
