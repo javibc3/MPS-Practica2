add4ad18a239826f6abdada009e959f872b657c0
lcc <lcc@lcc.lcc.es>
2022-03-17 11:29:25 +0100

    Cambio caso de prueba

--- src/test/java/DequeNodeTest.java
+++ src/test/java/DequeNodeTest.java
@@ -1,5 +1,16 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DequeNodeTest {
 
+    @Test
+    public void comprobarValorConNodoConValorYConectadoConAnteriorYPosterior(){
+        DequeNode<Integer> anterior = new DequeNode<>(1,null,null);
+        DequeNode<Integer> posterior = new DequeNode<>(2, null, null);
+        DequeNode<Integer> node = new DequeNode<>(1, anterior, posterior);
+        assertEquals(posterior,node.getNext());
+        assertEquals(anterior,node.getPrevious());
+        assertEquals(1,node.getItem());
+    }
 }
