e73a56254676716ccb1a2851af013674b08c0a27
Arthur <arthureboh@uma.es>
2022-03-17 11:42:07 +0100

    hecho 3 primeros tests

--- src/test/java/DequeNodeTest.java
+++ src/test/java/DequeNodeTest.java
@@ -14,4 +14,26 @@ class DequeNodeTest {
         assertEquals(expectedValue, obtainedValue);
     }
 
+    @Test
+    public void NodoTieneAnterior()
+    {
+        DequeNode<Integer> anterior = new DequeNode(1, null, null);
+        DequeNode<Integer> nodo = new DequeNode(2, null, anterior);
+
+        DequeNode<Integer> expectedValue = anterior;
+        DequeNode<Integer> obtainedValue = nodo.getPrevious();
+        assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void NodoTienePosterior()
+    {
+        DequeNode<Integer> posterior = new DequeNode(1, null, null);
+        DequeNode<Integer> nodo = new DequeNode(2, posterior, null);
+
+        DequeNode<Integer> expectedValue = posterior;
+        DequeNode<Integer> obtainedValue = nodo.getNext();
+        assertEquals(expectedValue, obtainedValue);
+    }
+
 }
