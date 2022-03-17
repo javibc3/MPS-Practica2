02bbb6d78184215983b007c17f8b52b948f52a58
lcc <lcc@lcc.lcc.es>
2022-03-17 11:57:27 +0100

    Creada clase DoubleEndedQueue

---
+++ src/main/java/DoubleEndedQueue.java
@@ -0,0 +1,37 @@
+public class DoubleEndedQueue<T> implements DoubleEndedQueuees<T> {
+
+    @Override
+    public void append(DequeNode<T> node) {
+
+    }
+
+    @Override
+    public void appendLeft(DequeNode<T> node) {
+
+    }
+
+    @Override
+    public void deleteFirst() {
+
+    }
+
+    @Override
+    public void deleteLast() {
+
+    }
+
+    @Override
+    public DequeNode<T> peekFirst() {
+        return null;
+    }
+
+    @Override
+    public DequeNode<T> peekLast() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
