dc53738ab3dbe5d347a130301380d56253cc4213
Arthur <arthureboh@uma.es>
2022-03-17 12:24:52 +0100

    Update DoubleLinkedListQueue.java

--- src/main/java/DoubleLinkedListQueue.java
+++ src/main/java/DoubleLinkedListQueue.java
@@ -1,16 +1,26 @@
 public class DoubleLinkedListQueue<T> implements DoubleEndedQueuees<T> {
 
-    DequeNode<T> principio;
-    DequeNode<T> fin;
+    private DequeNode<T> principio;
+    private DequeNode<T> fin;
+    private int size;
 
-    public DoubleLinkedListQueue(DequeNode<T> nodo){
+    public DoubleLinkedListQueue(DequeNode<T> nodo) {
         principio = nodo;
         fin = nodo;
+        size = (nodo != null) ? 1 : 0;
     }
 
     @Override
     public void append(DequeNode<T> node) {
-
+        if (node == null) throw new RuntimeException("El nodo que ha pasado es NULL");
+        if (size > 0) {
+            fin.setNext(node);
+            node.setPrevious(fin);
+        } else {
+            principio = node;
+        }
+        fin = node;
+        size++;
     }
 
     @Override
@@ -40,6 +50,6 @@ public class DoubleLinkedListQueue<T> implements DoubleEndedQueuees<T> {
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 }
