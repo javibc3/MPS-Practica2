9338339332c5228e45b3ac13de6509904832ef01
lcc <lcc@lcc.lcc.es>
2022-03-17 11:37:21 +0100

    Casos de prueba 7 a 9

--- src/test/java/DequeNodeTest.java
+++ src/test/java/DequeNodeTest.java
@@ -1,5 +1,34 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DequeNodeTest {
 
+    @Test
+    public void siNoTieneAnteriorEsPrimero() {
+        DequeNode<Integer> Primero = new DequeNode<>(5,null,null);
+        DequeNode<Integer> Ultimo = new DequeNode<>(6,null,Primero);
+        Primero.setNext(Ultimo);
+        assertTrue(Primero.isFirstNode());
+    }
+
+    @Test
+    public void siNoTienePosteriorEsUltimo() {
+        DequeNode<Integer> Primero = new DequeNode<>(5,null,null);
+        DequeNode<Integer> Ultimo = new DequeNode<>(6,null,Primero);
+        Primero.setNext(Ultimo);
+        assertTrue(Ultimo.isLastNode());
+    }
+
+    @Test
+    public void siTieneAnteriorYPosteriorNoEsNiPrimeroNiUltimo() {
+        DequeNode<Integer> Primero = new DequeNode<>(5,null,null);
+        DequeNode<Integer> Medio = new DequeNode<>(6,null,Primero);
+        DequeNode<Integer> Ultimo = new DequeNode<>(6,null,Medio);
+        Primero.setNext(Medio);
+        Medio.setNext(Ultimo);
+        Ultimo.setPrevious(Medio);
+        assertTrue(Medio.isNotATerminalNode());
+    }
 }
