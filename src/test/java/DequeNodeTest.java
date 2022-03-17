f3fb8e2f7d9412d7236e870c0a0ae5233f50da5f
lcc <lcc@lcc.lcc.es>
2022-03-17 11:40:31 +0100

    Añadida metodos de prueba en la clase DequeNodeTest

--- src/test/java/DequeNodeTest.java
+++ src/test/java/DequeNodeTest.java
@@ -8,9 +8,22 @@ class DequeNodeTest {
     public void comprobarValorConNodoConValorYConectadoConAnteriorYPosterior(){
         DequeNode<Integer> anterior = new DequeNode<>(1,null,null);
         DequeNode<Integer> posterior = new DequeNode<>(2, null, null);
-        DequeNode<Integer> node = new DequeNode<>(1, anterior, posterior);
+        DequeNode<Integer> node = new DequeNode<>(1, posterior, anterior);
         assertEquals(posterior,node.getNext());
         assertEquals(anterior,node.getPrevious());
         assertEquals(1,node.getItem());
+    }
+
+    @Test
+    public void comprobarCambioDeValorEnUnNodo(){
+        DequeNode<Integer> node = new DequeNode<>(1,null,null);
+        node.setItem(2);
+        assertEquals(2,node.getItem());
+    }
+
+    @Test
+    public void comprobarNodoConValorNulo(){
+        DequeNode<Integer> node = new DequeNode<>(null,null,null);
+        assertEquals(null,node.getItem());
     }
 }
