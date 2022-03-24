8b4845ddb8d01b4c21fb1dcc5c7ad21a45b64272
Arthur <arthureboh@uma.es>
2022-03-24 11:36:43 +0100

    creados test para 'append'

--- src/test/java/DoubleLinkedListQueueTest.java
+++ src/test/java/DoubleLinkedListQueueTest.java
@@ -5,12 +5,44 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DoubleLinkedListQueueTest {
     @Test
-    public void NodoSeCreaConValorCorrecto()
-    {
+    public void NodoSeCreaConValorCorrecto() {
         DequeNode<Integer> nodo = new DequeNode(2, null, null);
 
         int expectedValue = 2;
         int obtainedValue = nodo.getItem();
         assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void NodoSeAñadeAListaVacia() {
+        DoubleLinkedListQueue lista = new DoubleLinkedListQueue(null);
+        DequeNode<Integer> nodo = new DequeNode(2, null, null);
+        lista.append(nodo);
+        int expectedSize = 1;
+        int obtainedSize = lista.size();
+        DequeNode expectedValue = nodo;
+        DequeNode obtainedValue = lista.peekFirst();
+        assertEquals(expectedSize, obtainedSize);
+        assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void NodoSeAñadeALaIzquierdaDeListaNoVacia() {
+        DoubleLinkedListQueue lista = new DoubleLinkedListQueue(new DequeNode(3, null, null));
+        DequeNode<Integer> nodo = new DequeNode(2, null, null);
+        lista.append(nodo);
+        int expectedSize = 2;
+        int obtainedSize = lista.size();
+        DequeNode expectedValue = nodo;
+        DequeNode obtainedValue = lista.peekLast();
+        assertEquals(expectedSize, obtainedSize);
+        assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void AñadirNodoNullLanzaExcepcion()
+    {
+        DoubleLinkedListQueue lista = new DoubleLinkedListQueue(new DequeNode(3, null, null));
+        assertThrows(RuntimeException.class, () -> lista.append(null));
     }
 }
